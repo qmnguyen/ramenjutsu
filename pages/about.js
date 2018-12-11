d39ae0710ccb7a026890c87cd36d07910e2fcc0d
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2018-12-11 15:57:19 -0500

    Restructuring code on next.js instead of gatsby.js

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <h1>About ramenjutsu</h1>
+  </Layout>
+)
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+const Index = () => (
+  <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
+)
+
+export default Index
