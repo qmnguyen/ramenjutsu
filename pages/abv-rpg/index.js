655206579d69574977b6d5cd7d402b963e40b4c3
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2019-02-25 16:28:59 -0500

    Took the auth code out of ABV RPG main page.

--- pages/abv-rpg/index.js
+++ pages/abv-rpg/index.js
@@ -15,14 +15,6 @@ const Index = (props) => (
     }}>
       <a className="login-link" target="_blank">Login</a>
     </Link>
-    <ul>
-      {props.beers.map(({beer}) => (
-        <li key={beer.bid}>
-          <img src={beer.beer_label} alt={beer.beer_name} />
-          <a>{beer.beer_name}</a>
-        </li>
-      ))}
-    </ul>
     <style jsx>{`
       h1 {
         margin: 40px auto 20px;
@@ -34,48 +26,8 @@ const Index = (props) => (
         text-decoration: none;
         text-transform: uppercase;
       }
-      ul {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        list-style-type: none;
-        margin-left: 0;
-        padding-left: 0;
-      }
-      li {
-        align-content: center;
-        background: #ffffff;
-        border: 1px solid #dddddd;
-        border-radius: 4px;
-        display: flex;
-        flex-direction: column;
-        height: 250px;
-        justify-content: center;
-        margin: 0 0 40px;
-        padding: 20px;
-        text-align: center;
-        width: 250px;
-      }
-      li img {
-        margin: 0 auto 15px;
-        max-width: 150px;
-      }
-      li a {
-        display: block;
-        line-height: 1.25;
-      }
     `}</style>
   </DashLayout>
 )
-
-Index.getInitialProps = async function(context) {
-  const res = await fetch(`https://api.untappd.com/v4/user/checkins?access_token=${process.env.ACCESS_TOKEN}`)
-
-  const data = await res.json()
-
-  return {
-    beers: data.response.checkins.items
-  }
-}
 
 export default Index
--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -1,10 +1,57 @@
 const ABVRPGCallback = (props) => (
-  <div>
-    <h1>Start drinking!</h1>
-  </div>
+  <ul>
+    {props.beers.map(({beer}) => (
+      <li key={beer.bid}>
+        <img src={beer.beer_label} alt={beer.beer_name} />
+        <a>{beer.beer_name}</a>
+      </li>
+    ))}
+  </ul>
+  <style jsx>{`
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-evenly;
+      list-style-type: none;
+      margin-left: 0;
+      padding-left: 0;
+    }
+    li {
+      align-content: center;
+      background: #ffffff;
+      border: 1px solid #dddddd;
+      border-radius: 4px;
+      display: flex;
+      flex-direction: column;
+      height: 250px;
+      justify-content: center;
+      margin: 0 0 40px;
+      padding: 20px;
+      text-align: center;
+      width: 250px;
+    }
+    li img {
+      margin: 0 auto 15px;
+      max-width: 150px;
+    }
+    li a {
+      display: block;
+      line-height: 1.25;
+    }
+  `}</style>
 )
 
 // props.url.asPath contains the code sent back from Untappd API
 // redirecturl/?code=CODE
+
+ABVRPGCallback.getInitialProps = async function(context) {
+  const res = await fetch(`https://api.untappd.com/v4/user/checkins?access_token=${process.env.ACCESS_TOKEN}`)
+
+  const data = await res.json()
+
+  return {
+    beers: data.response.checkins.items
+  }
+}
 
 export default ABVRPGCallback
