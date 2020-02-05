9587213bf7636516b08189fbf27ea8aa34339775
Quan Nguyen <quannguyen@QuanNguyensMBP2.fios-router.home>
2020-02-05 15:32:00 -0500

    Adding back env var.

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,12 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const Index = (props) => {
+  const authBaseUrl = process.env.AUTH_BASE_URL;
 
   return (
     <Layout>
       <br />
-      <Link href="/">
+      <Link href={authBaseUrl}>
         <a>Login with your Untappd account</a>
       </Link>
     </Layout>
