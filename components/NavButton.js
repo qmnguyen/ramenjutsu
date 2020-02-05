d480fcc43fd5c78e6a7b19b99519dcd7be91c636
Quan Nguyen <quannguyen@QuanNguyensMBP2.fios-router.home>
2020-02-04 21:45:27 -0500

    Active classing using withRouter.

--- components/NavButton.js
+++ components/NavButton.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import { withRouter } from "next/router";
 
 import "./NavButton.scss";
 
 const NavButton = (props) => (
   <Link href={props.path}>
-    <div className="NavButton">
+    <div className={`NavButton ${props.router.pathname === props.path ? "active" : ""}`}>
       <div className="Icon">{props.icon}</div>
       <span className="Label">{props.label}</span>
     </div>
   </Link>
 )
 
-export default NavButton;
+export default withRouter(NavButton);
