4780f565185d27c6fa05743816bcd83a46ec607b
Quan Nguyen <quannguyen@QuanNguyensMBP2.fios-router.home>
2020-02-05 15:15:03 -0500

    Testing a removal of env variable for url in Link module.

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,11 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const Index = (props) => {
-  const authUrl = process.env.AUTH_BASE_URL;
 
   return (
     <Layout>
       <br />
-      <Link href={authUrl}>
+      <Link href="/">
         <a>Login with your Untappd account</a>
       </Link>
     </Layout>
