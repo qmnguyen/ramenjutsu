b88b17921ac37d4783aa929513c6830bbe2e6b60
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2020-02-24 16:07:38 -0500

    Test for code after data fetch.

--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -12,6 +12,10 @@ const GetAccessToken = (props) => {
 }
 
 GetAccessToken.getInitialProps = async (cxt) => {
+
+  // testing if code is pulled in
+  { console.log(cxt.query.code) }
+
   const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
 
   const data = await res.json();
