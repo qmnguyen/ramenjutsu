a91dbd8f0277d86409c29936c777b046fc92be74
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2020-02-24 16:10:16 -0500

    Testing code query again.

--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -14,7 +14,7 @@ const GetAccessToken = (props) => {
 GetAccessToken.getInitialProps = async (cxt) => {
 
   // testing if code is pulled in
-  { console.log(cxt.query.code) }
+  { console.log(`the code query:` + cxt.query.code) }
 
   const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
 
