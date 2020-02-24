67b62ce29ce804486c43db43ed5b04d59c83407e
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2020-02-24 16:26:14 -0500

    Moar changes to abvrpgcallback testing code and accessToken

--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -5,23 +5,20 @@ const codeQuery = [];
 const customAccessToken = [];
 
 const GetAccessToken = (props) => {
-  console.log(props.accessToken);
   return (
-    <h1>hello world: {props.accessToken}</h1>
+    <h1>hello world: {props.accessToken} and {props.specialCode}</h1>
   )
 }
 
 GetAccessToken.getInitialProps = async (cxt) => {
 
-  // testing if code is pulled in
-  { console.log(`the code query:` + cxt.query.code) }
-
   const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
 
-  const data = await res.json();
-  { console.log(data.response.access_token) }
+  // const data = await res.json();
+
   return {
-    accessToken: data.response.access_token
+    accessToken: res.response.access_token,
+    specialCode: cxt.query.code
   }
 }
 
