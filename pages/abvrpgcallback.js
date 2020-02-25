07aa21b970b2acf341e879820ae2686f79206e40
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2020-02-24 20:17:53 -0500

    Forgot to save last change commit.

--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -8,7 +8,12 @@ const GetAccessToken = (props) => {
 
 GetAccessToken.getInitialProps = async (cxt) => {
 
-  const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
+  // const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
+
+
+  // Replace code test
+  const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=4D64A73A1E7AB8473B77E0A3C7583FCE9BF3AE9B`);
+
 
   const data = await res.json();
 
