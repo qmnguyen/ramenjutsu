391a514e54a791825b67805e027b4fee370f3297
Quan Nguyen <quannguyen@Quan-Nguyens-MacBook-Pro-2.local>
2020-02-24 20:15:22 -0500

    Trying a hard-coded code.

--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -1,22 +1,20 @@
-import { withRouter } from "next/router";
 import fetch from "isomorphic-unfetch";
 
 const GetAccessToken = (props) => {
   return (
-    <h1>The code is {props.specialCode}</h1>
+    <h1>Okay here goes nothing {props.theAccessToken}</h1>
   )
 }
 
 GetAccessToken.getInitialProps = async (cxt) => {
-  const myCode = cxt.query.code
 
-  // const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
-  //
-  // const data = await res.json();
+  const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
+
+  const data = await res.json();
 
   return {
-    specialCode: myCode
+    theAccessToken: data.response.access_token
   }
 }
 
-export default withRouter(GetAccessToken);
+export default GetAccessToken;
