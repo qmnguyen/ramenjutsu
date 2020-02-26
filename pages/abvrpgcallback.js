0e0634f8c7ae87a2cf71f1b4d4b2c0f7bb292025
Quan Nguyen <quannguyen@QuanNguyensMBP2.fios-router.home>
2020-02-26 16:38:11 -0500

    Making changes from abvrpg subdomain to experience subdomain

--- pages/abvrpgcallback.js
+++ pages/abvrpgcallback.js
@@ -2,24 +2,21 @@ import fetch from "isomorphic-unfetch";
 
 const GetAccessToken = (props) => {
   return (
-    <h1>Okay here goes nothing {props.theAccessToken}</h1>
+    <h1>Access Token: {props.theAccessToken} and Code: {props.theCode}</h1>
   )
 }
 
-GetAccessToken.getInitialProps = async (cxt) => {
+GetAccessToken.getInitialProps = async ({ query }) => {
 
-  // const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${cxt.query.code}`);
-
-
-  // Replace code test
-  const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=4D64A73A1E7AB8473B77E0A3C7583FCE9BF3AE9B`);
-
+  const res = await fetch(`https://untappd.com/oauth/authorize/?client_id=`+process.env.CLIENTID+`&client_secret=`+process.env.CLIENT_SECRET+`&response_type=code&redirect_url=`+process.env.REDIRECT_URL+`&code=${query.code}`);
 
   const data = await res.json();
 
   return {
-    theAccessToken: data.response.access_token
+    theAccessToken: data.response.access_token,
+    theCode: query.code
   }
+
 }
 
 export default GetAccessToken;
